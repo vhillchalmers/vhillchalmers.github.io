e9d89a9b366bbeb447010912b06a1f0dd6686420
Victoria Hill-Chalmers <v.hill-chalmers@live.co.uk>
2017-03-14 21:35:56 +0000

    added 2 modals

---
+++ assets/js/modal.js
@@ -0,0 +1,18 @@
+function showModal(id){
+	var modal = document.getElementById(id);
+	
+	modal.style.display = "block";
+	var span = modal.getElementsByClassName("close")[0];
+	
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+}
